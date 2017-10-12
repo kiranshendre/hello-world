93dd6c33392a424257b0d144d2cdafc9a5967ab9
Kiran Shendre <kirangshendre@gmail.com>
2017-10-12 22:51:23 +0530

    Added new java file for testing push

---
+++ ExceptionTest.java
@@ -0,0 +1,18 @@
+public class ExceptionTest{
+	public static void main(String args[]){
+		try{
+			int a = 15;
+			System.out.println(15/0);		
+		}
+		catch(ArithmeticException e){
+			System.out.println(e);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		finally{
+			System.out.println("Finally bloack executed");
+		}
+		System.out.println("Rest of the code");
+	}
+}
